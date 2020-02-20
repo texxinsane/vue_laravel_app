8d213abb5b922234240716de5961f5aed020ca1b
Ionut Mihaescu <ionut.mihaescu@gmail.com>
2020-02-20 01:42:40 +0100

    some updates

---
+++ tests/Integration/FlightControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Integration;
+
+
+use Tests\TestCase;
+
+class FlightControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function get_quote()
+    {
+
+    }
+}
---
+++ tests/Services/RapidApiServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+
+use Tests\TestCase;
+
+class RapidApiServiceTest extends TestCase
+{
+    public function get_quote_success()
+    {
+
+    }
+}
